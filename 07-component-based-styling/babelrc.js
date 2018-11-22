fa762082739fa30ce82647b8cd87f031a88bbffb
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2018-11-22 09:03:34 +0100

    fix Step 07, fix webpack.config, update dependencies

--- 07-component-based-styling/babelrc.js
+++ 07-component-based-styling/babelrc.js
@@ -2,7 +2,10 @@ const sass = require('node-sass');
 const path = require('path');
 
 module.exports = function processSass(data, filename) {
-  // we need to remove the ~ that was used for the sass loader, otherwise the path with include paths is wrong.
+  /**
+   * we need to remove the ~ that was used for the sass loader,
+   * otherwise the path with include paths is wrong.
+   */
   const removeTilde = data.replace(/~/g, '');
   const result = sass.renderSync({
     data: removeTilde,
